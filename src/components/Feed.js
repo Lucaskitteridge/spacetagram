d3148de64607fa4b9133dd03ab93c9f2117b886b
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-04-20 14:53:22 -0400

    fetching data and adding to useEffect

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 export default function Feed() {
+  const [photosOfTheDay, setPhotosOfTheDay] = useState([]);
   const apiKey = process.env.REACT_APP_API_KEY || "DEMO_KEY";
 
   //Fetch data from Nasa Api
-  const fetchNasaData = () => {
-    fetch(`https://api.nasa.gov/planetary/apod?&api_key=${apiKey}`)
+  const fetchNasaData = (nasaKey) => {
+    fetch(`https://api.nasa.gov/planetary/apod?&api_key=${nasaKey}`)
       .then((response) => {
-        console.log(response);
         if (response.ok) {
           return response.json();
+        } else {
+          console.log(response)
         }
       })
-      .then((data) => console.log(data))
+      .then((data) => setPhotosOfTheDay(data))
       .catch((err) => {
         console.log(err);
       });
   };
 
-  fetchNasaData();
+  useEffect(() => {
+    fetchNasaData(apiKey);
+    console.log(photosOfTheDay)
+  }, []);
 
   return <div className="feed"></div>;
 }
