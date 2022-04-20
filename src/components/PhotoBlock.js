0e571c3c5f2cf128fe491a217bce9108aad8739d
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-04-20 16:09:44 -0400

    removed animation library and added css

--- src/components/PhotoBlock.js
+++ src/components/PhotoBlock.js
@@ -1,24 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import { Button } from "react-bootstrap";
+import "./PhotoBlock.css";
 
-export default function PhotoBlock(props) {
-  const {url, title, date, explanation } = props.photo
+export default function PhotoBlock({ photo }) {
+  const { url, title, date, explanation } = photo;
   return (
     <div className="photoBlock">
       <div>
-        <img
-          src={url}
-          alt={title}
-          className="spacePhoto"
-        />
+        <img src={url} alt={title} className="spacePhoto" />
       </div>
       <div className="descriptionText">
         <div className="dateAndTitle">
           {title} - {date}
         </div>
-        <div className="explanation">
-          Explanation: {explanation}
-        </div>
+        <div className="explanation">Explanation: {explanation}</div>
         <div className="likeButton">
           <Button variant="outline-success">
             Like
