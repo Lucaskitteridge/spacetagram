b607d9fc7b10e1170069c020bf5972700b1f5c08
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-04-21 14:16:05 -0400

    cortrect infinite scroll working

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./Feed.css";
 import InfiniteScroll from "react-infinite-scroller";
-import moment from 'moment'
+import moment from "moment";
 import PhotoBlock from "./PhotoBlock";
 export default function Feed() {
   const apiKey = process.env.REACT_APP_API_KEY || "DEMO_KEY";
   const [photosOfTheDay, setPhotosOfTheDay] = useState([]);
-  const [startDate, setStartDate] = useState(moment().subtract(5, 'days').format().slice(0, 10));
+  const [startDate, setStartDate] = useState(
+    moment().subtract(5, "days").format().slice(0, 10)
+  );
   const [endDate, setEndDate] = useState(moment().format().slice(0, 10));
 
   //Fetch data from Nasa Api
   const fetchNasaData = (nasaKey, start, end) => {
     fetch(
-      `https://api.nasa.gov/planetary/apod?start_date=${start
-        }&end_date=${end}&api_key=${nasaKey}`
+      `https://api.nasa.gov/planetary/apod?start_date=${start}&end_date=${end}&api_key=${nasaKey}`
     )
       .then((response) => {
         if (response.ok) {
@@ -24,11 +25,15 @@ export default function Feed() {
       })
       .then((data) => {
         if (photosOfTheDay.length > 0) {
-          let newData = data.reverse()
+          let newData = data.reverse();
           setPhotosOfTheDay((prev) => [...prev, newData].flat());
         } else {
           setPhotosOfTheDay(data.reverse());
         }
+        let newStart = moment(startDate).subtract(6, "days").format().slice(0, 10);
+        let newEnd = moment(endDate).subtract(6, "days").format().slice(0, 10);
+        setStartDate(newStart);
+        setEndDate(newEnd);
       })
       .catch((err) => {
         console.log(err);
@@ -40,10 +45,7 @@ export default function Feed() {
   }, []);
 
   const fetchMorePhotos = () => {
-    console.log('here')
     setTimeout(() => {
-      setStartDate(moment(startDate).subtract(6, 'days').format().slice(0, 10))
-      setEndDate(moment(endDate).subtract(6, 'days').format().slice(0, 10))
       fetchNasaData(apiKey, startDate, endDate);
     }, 1500);
   };
