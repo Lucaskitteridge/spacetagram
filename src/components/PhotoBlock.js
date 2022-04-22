feade9583f1f80a7e6a1fba4d1434138d858e66f
Lucas Kitteridge <lucas.benjamin.kitteridge@gmail.com>
2022-04-22 18:22:32 -0400

    remembering likes on refresh using localstorage setup'

--- src/components/PhotoBlock.js
+++ src/components/PhotoBlock.js
@@ -1,14 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import "./PhotoBlock.css";
 
 export default function PhotoBlock({ photo }) {
   const { url, title, date, explanation } = photo;
-  const [like, setLike] = useState(false)
+  const [selectedPhoto, setSelectedPhoto] = useState(photo);
+  let likedInStorage = localStorage.getItem(selectedPhoto.date);
 
-  const onLike = function() {
-    setLike(!like)
-  }
+  //Uses likes in local storage to tell if the photo is previously liked
+  useEffect(() => {
+    if (likedInStorage === "false") {
+      setSelectedPhoto((prevPhoto) => {
+        return {
+          ...prevPhoto,
+          liked: false,
+        };
+      });
+    } else if (likedInStorage === "true") {
+      setSelectedPhoto((prevPhoto) => {
+        return {
+          ...prevPhoto,
+          liked: true,
+        };
+      });
+    }
+  }, []);
+
+  //Sets local storage for likes
+  const onLike = () => {
+    if (!selectedPhoto.liked) {
+      setSelectedPhoto((prevPhoto) => {
+        return {
+          ...prevPhoto,
+          liked: true,
+        };
+      });
+      localStorage.setItem(selectedPhoto.date, true);
+    } else {
+      setSelectedPhoto((prevPhoto) => {
+        return {
+          ...prevPhoto,
+          liked: false,
+        };
+      });
+      localStorage.setItem(selectedPhoto.date, false);
+    }
+  };
+  
 
   return (
     <div className="photoBlock">
@@ -22,7 +60,7 @@ export default function PhotoBlock({ photo }) {
         <div className="explanation">{explanation}</div>
         <div className="likeButton">
           <Button variant="outline-success" onClick={onLike}>
-            {like ? 'Unlike' : "Like"}
+            {selectedPhoto.liked ? 'Like' : "Unlike"}
           </Button>
         </div>
       </div>
